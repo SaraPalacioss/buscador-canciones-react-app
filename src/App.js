823cf8643246019c97bc322f451c15c1f5ed0282
sarastackhouse <sarapalacios7@gmail.com>
2021-02-04 19:53:48 +0100

    API for artist info and bio applied

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {Fragment, useState, useEffect} from 'react';
 import axios from 'axios';
 import Formulario from './components/Formulario';
+import Cancion from './components/Cancion';
+import Info from './components/Info';
+
 
 function App() {
 
@@ -8,6 +11,7 @@ function App() {
 
   const [busquedaLetra, guardarBusquedaLetra] = useState({});
   const [letra, guardarLetra] = useState('');
+  const [info, guardarInfo] = useState({});
 
   useEffect(() => {
    if(Object.keys(busquedaLetra).length === 0) return;
@@ -15,12 +19,20 @@ function App() {
    const consultarAPILetra = async ()=> {
       const {artista, cancion} = busquedaLetra;
       const URL =`https://api.lyrics.ovh/v1/${artista}/${cancion}`;
-      const resultado = await axios.get(URL)
+      const URL2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+      
+      const [letra, informacion] = await Promise.all([
+        axios.get(URL),
+        axios.get(URL2)
+      ]);
 
-      guardarLetra(resultado.data.lyrics);
+      guardarLetra(letra.data.lyrics);
+      guardarInfo(informacion.data.artists[0]);
+
+      // guardarLetra(resultado.data.lyrics);
     }
    consultarAPILetra();
-  }, [busquedaLetra])
+  }, [busquedaLetra, info])
 
 
   return (
@@ -28,6 +40,20 @@ function App() {
       <Formulario 
         guardarBusquedaLetra={guardarBusquedaLetra}
       />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <Info
+              info={info}
+            />
+          </div>
+          <div className="col-md-6">
+            <Cancion
+              letra={letra}
+            />
+            </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
